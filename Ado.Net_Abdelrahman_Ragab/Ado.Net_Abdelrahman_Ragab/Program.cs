be8369a349bd8035bca85b4b60f9754dabdb1c19
Abdellateef Eid <abdellateef35@gmail.com>
2024-10-10 23:49:52 +0300

    retrieve the data using SqlDataReader

--- Ado.Net_Abdelrahman_Ragab/Ado.Net_Abdelrahman_Ragab/Program.cs
+++ Ado.Net_Abdelrahman_Ragab/Ado.Net_Abdelrahman_Ragab/Program.cs
@@ -19,14 +19,38 @@ namespace Ado.Net_Abdelrahman_Ragab
                                              "Encrypt=true;" +
                                              "TrustServerCertificate=true;";
 
+
+            #region retrieve the data using SqlDataReader
+
+            // Create a new SqlCommand object to execute SQL queries
+            SqlCommand sqlCommand = new SqlCommand();
+
             // Open the SQL connection to the database
             sqlConnection.Open();
 
             // display the connection state in the console (if needed)
-            Console.WriteLine(sqlConnection.State);
+            // Console.WriteLine(sqlConnection.State);
+
+            // Set the SQL command text to select Top_Id and Top_Name columns from the Topic table
+            sqlCommand.CommandText = "SELECT Top_Id, Top_Name FROM Topic;";
+
+            // Assign the open SqlConnection to the SqlCommand
+            sqlCommand.Connection = sqlConnection;
+
+            // Execute the SQL query and retrieve the data using SqlDataReader
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            // Iterate through the retrieved rows
+            while (sqlDataReader.Read())
+            {
+                // Output the Top_Id and Top_Name columns of each row to the console
+                Console.WriteLine($"Top_Id: {sqlDataReader["Top_Id"]}\t Top_Name: {sqlDataReader["Top_Name"]}");
+            }
 
             // Close the SQL connection after the operation is done
             sqlConnection.Close();
+
+            #endregion
 
         }
     }
