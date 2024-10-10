2b16f0a2e1a981c5e578b24d58b3bb17affa1be6
Abdellateef Eid <abdellateef35@gmail.com>
2024-10-10 23:41:16 +0300

    initial commet

---
+++ Ado.Net_Abdelrahman_Ragab/Ado.Net_Abdelrahman_Ragab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ado.Net_Abdelrahman_Ragab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
